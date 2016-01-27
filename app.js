e838a409d40f35e67e00a85248ce3e7abed24662
TJ Egan <twegan@us.ibm.com>
2016-01-26 21:07:11 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// FINAL COUNTDOWN
+var doomsday = 'February 26 2016 00:00:00 GMT-06:00';
+
+function countdownToDoomsday(doomsday) {
+  var totalTime = Date.parse(doomsday) - Date.parse(new Date());
+  var seconds = Math.floor( (totalTime/1000) % 60 );
+  var minutes = Math.floor( (totalTime/1000/60) % 60 );
+  var hours = Math.floor( (totalTime/(1000*60*60)) % 24 );
+  var days = Math.floor( totalTime/(1000*60*60*24) );
+
+  return {
+    'total': totalTime,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeDoomsday(id, doomsday) {
+  var clock = document.getElementById(id);
+  var timeInterval = setInterval(function() {
+    var t = countdownToDoomsday(doomsday);
+    clock.innerHTML = 'days: ' + t.days + '<br>' +
+                      'hours: ' + t.hours + '<br>' +
+                      'minutes: ' + t.minutes + '<br>' +
+                      'seconds: ' + t.seconds;
+    if (t.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+initializeDoomsday('clock', doomsday);
